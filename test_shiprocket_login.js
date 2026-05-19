4be6b658796fe93351674b96ea20302fef2e0823
Saifudheen <saifudheenss369@gmail.com>
2026-05-19 23:02:17 +0530

    test: update shiprocket login verification script

--- test_shiprocket_login.js
+++ test_shiprocket_login.js
@@ -1,6 +1,8 @@
-const email = "info@kosemperfume.com";
-const password = "Fx2a8@1Avhr!t^jXpF2tL#D4NBIY4PPk";
-const url = "https://apiv2.shiprocket.in/v1/payload/user/login";
+require('dotenv').config();
+
+const email = process.env.SHIPROCKET_EMAIL;
+const password = process.env.SHIPROCKET_PASSWORD;
+const url = "https://apiv2.shiprocket.in/v1/external/auth/login";
 
 async function test() {
     try {
@@ -11,7 +13,7 @@ async function test() {
             body: JSON.stringify({ email, password })
         });
 
-        console.log(`Status status: ${response.status} ${response.statusText}`);
+        console.log(`Status: ${response.status} ${response.statusText}`);
         const data = await response.json();
         console.log(JSON.stringify(data, null, 2));
     } catch (err) {
